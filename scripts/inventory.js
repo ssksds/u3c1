5fee85fc43a0889ef938adc65a9f0442eb29d1b0
ssksds <bsriramsds@gmail.com>
2022-05-24 08:05:55 +0530

    Update inventory.js

--- scripts/inventory.js
+++ scripts/inventory.js
@@ -20,49 +20,58 @@ data.forEach(function(e,index){
     price.innerText=e.price;
     let btn=document.createElement("button")
     btn.innerText="remove"
-    
+    btn.setAttribute("id",'remove_product')
     btn.addEventListener("click",function(){
-        remove(index)
+        remove(e,index)
     });
 
     div.append(image,type,desc,price,btn)
 
     container.append(div)
 })
+function remove(e,i){
+    data.splice(i,1)
+    localStorage.setItem("products",JSON.stringify(data))
+    window.location.reload()
+    }
+    }
 
-//     })
-}
+
+// function remove(index) {
+//     newData.splice(newData.indexOf(index), 1);
+//     localStorage.setItem("products", JSON.stringify(newData));
+//     window.location.reload(true);
+//   }
 
 
 
 
 // function remove(index){
-// let data=JSON.parse(localStorage.getItem("products"))||[]
-// let newproducts=data.filter(function(el,i){
-//     if(i===index){
-//        let trash=JSON.parse(localStorage.getItem("trash"))||[]
-//         trash.push(el)
-//         localStorage.setItem("trash",JSON.stringify(trash))
-//         window.location.reload()
-//     }
-// })
-// localStorage.setItem("products",JSON.stringify(arr))
+//     let data=JSON.parse(localStorage.getItem("products"))||[];
+//     let newData=data.filter(function(el,i){
+//         if(i===index){
+//             let trash=JSON.parse(localStorage.getItem("trash"))||[];
+//             trash.push(el)
+//             localStorage.setItem("trash",JSON.stringify(trash))
+//             window.location.reload()
+//         }else{
+//             return i !==index;
+//         }
+//      });
+
+//      localStorage.setItem("products", JSON.stringify(newData));
+     
 // }
 
-function remove(index){
-    let data=JSON.parse(localStorage.getItem("products"))||[];
-    let newData=data.filter(function(el,i){
-        if(i===index){
-            let trash=JSON.parse(localStorage.getItem("trash"))||[];
-            trash.push(el)
-            localStorage.setItem("trash",JSON.stringify(trash))
-            window.location.reload()
-        }else{
-            return i !==index;
-        }
-     });
 
-     localStorage.setItem("products", JSON.stringify(newData));
-     
-}
+// function remove() {
+//     let data = JSON.parse(localStorage.getItem("products"))||[];
+//     console.log(data)
+//     // for (var index = 0; index < data.length; index++) {
+//     //   data.splice(index, 1);
+//     //   window.location.reload()
+//     // }
+//     // localStorage.setItem("products", JSON.stringify(newData));
+    
+//   }
 append()
